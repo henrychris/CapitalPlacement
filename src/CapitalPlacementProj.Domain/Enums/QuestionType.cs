07e560b5a3c4aaf08a32d5c9619a91641dc20fe9
Henry Ihenacho <henrychris2003@gmail.com>
2024-05-15 18:04:05 +0100

    add question type enum

---
+++ src/CapitalPlacementProj.Domain/Enums/QuestionType.cs
@@ -0,0 +1,29 @@
+﻿namespace CapitalPlacementProj.Domain.Enums
+{
+    public enum QuestionType
+    {
+        Paragraph,
+        YesNo,
+        Dropdown,
+        Date,
+        Number,
+        MultipleChoice
+    }
+
+    public static class QuestionTypeExtensions
+    {
+        public static string ToFriendlyString(this QuestionType me)
+        {
+            return me switch
+            {
+                QuestionType.Paragraph => "Paragraph",
+                QuestionType.YesNo => "YesNo",
+                QuestionType.Dropdown => "Dropdown",
+                QuestionType.Date => "Date",
+                QuestionType.Number => "Number",
+                QuestionType.MultipleChoice => "MultipleChoice",
+                _ => "Unknown",
+            };
+        }
+    }
+}
