7980189d0e84262cb5c5b998103f7e31fbf0d595
Henry Ihenacho <henrychris2003@gmail.com>
2024-05-15 18:39:49 +0100

    implement create questionnaire

--- src/CapitalPlacementProj.Application/Interfaces/IQuestionnaireService.cs
+++ src/CapitalPlacementProj.Application/Interfaces/IQuestionnaireService.cs
@@ -1,4 +1,11 @@
-﻿namespace CapitalPlacementProj.Application.Interfaces
+﻿using CapitalPlacementProj.Application.Features.CreateQuestionnaire;
+using CapitalPlacementProj.Domain.Entities;
+
+namespace CapitalPlacementProj.Application.Interfaces
 {
-    public interface IQuestionnaireService { }
+    public interface IQuestionnaireService
+    {
+        Task InitializeAsync();
+        Task<Questionnaire?> CreateQuestionnaireAsync(CreateQuestionnaireRequest request);
+    }
 }
--- src/CapitalPlacementProj.Infrastructure/Services/QuestionnaireService.cs
+++ src/CapitalPlacementProj.Infrastructure/Services/QuestionnaireService.cs
@@ -1,6 +1,59 @@
-﻿using CapitalPlacementProj.Application.Interfaces;
+﻿using CapitalPlacementProj.Application.Features.CreateQuestionnaire;
+using CapitalPlacementProj.Application.Interfaces;
+using CapitalPlacementProj.Application.Interfaces.Repositories;
+using CapitalPlacementProj.Domain.Entities;
+using CapitalPlacementProj.Domain.Enums;
+using Microsoft.Extensions.Logging;
 
 namespace CapitalPlacementProj.Infrastructure.Services
 {
-    public class QuestionnaireService : IQuestionnaireService { }
+    public class QuestionnaireService(
+        IQuestionnaireRepository questionnaireRepository,
+        ILogger<QuestionnaireService> logger
+    ) : IQuestionnaireService
+    {
+        public async Task InitializeAsync()
+        {
+            await questionnaireRepository.InitializeAsync();
+        }
+
+        public async Task<Questionnaire?> CreateQuestionnaireAsync(
+            CreateQuestionnaireRequest request
+        )
+        {
+            var questionObjects = request
+                .Questions.Select(q => new QuestionObject
+                {
+                    Question = q.Question,
+                    QuestionType = ConvertToEnum(q.QuestionType),
+                    Choices = q.Choices,
+                    MaximumChoices = q.MaximumChoices,
+                })
+                .ToList();
+
+            var survey = new Questionnaire
+            {
+                ProgramName = request.ProgramName,
+                ProgramDescription = request.ProgramDescription,
+                PersonalInformation = request.PersonalInfo,
+                Questions = questionObjects
+            };
+
+            return await questionnaireRepository.CreateQuestionnaireAsync(survey);
+        }
+
+        private static QuestionType ConvertToEnum(string questionType)
+        {
+            return questionType switch
+            {
+                "Paragraph" => QuestionType.Paragraph,
+                "YesNo" => QuestionType.YesNo,
+                "Dropdown" => QuestionType.Dropdown,
+                "MultipleChoice" => QuestionType.MultipleChoice,
+                "Date" => QuestionType.Date,
+                "Number" => QuestionType.Number,
+                _ => throw new ArgumentException("Invalid question type")
+            };
+        }
+    }
 }
