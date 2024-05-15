2d9a96f180c77cc12705ca4a46c4859e7d6fdd76
Henry Ihenacho <henrychris2003@gmail.com>
2024-05-15 18:39:28 +0100

    add endpoint to init project

---
+++ src/CapitalPlacementProj/Endpoints/Init/InitializeEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using CapitalPlacementProj.Application.Interfaces;
+
+namespace CapitalPlacementProj.Endpoints.Init
+{
+    public class InitializeEndpoint(IQuestionnaireService questionnaireService)
+        : EndpointWithoutRequest
+    {
+        public override void Configure()
+        {
+            Post("/initialise");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            await questionnaireService.InitializeAsync();
+            await SendOkAsync(ct);
+        }
+    }
+}
